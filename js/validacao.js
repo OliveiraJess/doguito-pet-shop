f5688af07cc1711a7f31dc82f3201ab6fe90e357
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-06-06 15:11:20 -0300

    validando cpf

--- js/validacao.js
+++ js/validacao.js
@@ -89,7 +89,7 @@ function validaCPF(input) {
     const cpfFormatado = input.value.replace(/\D/g, "");
     let mensagem = "";
 
-    if (!checaCPFRepetido(cpfFormatado)) {
+    if (!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
         mensagem = "O CPF digitado não é válido";
     }
 
@@ -119,5 +119,42 @@ function checaCPFRepetido(cpf) {
     })
 
     return cpfValido;
+
+}
+
+function checaEstruturaCPF(cpf) {
+    const multiplicador = 10;
+
+    return checaDigitoVerificador(cpf, multiplicador)
+
+}
+
+function checaDigitoVerificador(cpf, multiplicador) {
+
+    if (multiplicador >= 12) {
+        return true
+    }
+
+    let multiplicadorInicial = multiplicador
+    let soma = 0;
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('');
+    const digitoVerificador = cpf.charAt(multiplicador - 1)
+
+    for (let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--) {
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++
+    }
+
+    if (digitoVerificador == confirmaDigito(soma)) {
+
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+
+    return false
+}
+
+function confirmaDigito(soma) {
+
+    return 11 - (soma % 11)
 
 }
